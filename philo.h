316c3a037b4ad7bf06f49e704073a36303a54c03
Aqil Adenan <aadenan@c2r6s6.42singapore.sg>
2024-06-18 14:55:26 +0800

    Added header file

---
+++ philo.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aadenan <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/18 14:48:39 by aadenan           #+#    #+#             */
+/*   Updated: 2024/06/18 14:52:24 by aadenan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// LIBFT IS NOT AUTHORIZED
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <sys/time.h>
+// #include <sys/types.h> // To check again
+// #include <sys/wait.h> // To check again
+
+typedef struct s_data
+{
+	// TO DO
+} t_data
+
