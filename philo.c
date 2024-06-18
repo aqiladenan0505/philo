a545f91da52bbd41d26b35dd339ddc8c04e5a077
Aqil Adenan <aadenan@c2r6s6.42singapore.sg>
2024-06-18 14:58:06 +0800

    Added printf for if statement

--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: aadenan <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 12:12:14 by aadenan           #+#    #+#             */
-/*   Updated: 2024/06/18 14:54:24 by aadenan          ###   ########.fr       */
+/*   Updated: 2024/06/18 14:57:38 by aadenan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,14 @@ int	main(int argc, char **argv)
 	if (argc == 5 || argc == 6)
 	{
 		parse_input(argv);
+		printf("Input is correct\n");
 		// data_init();
 		// execute_philo();
 		// free_all();
 	}
 	else
 	{
+		printf("Wrong number of arguments\n");
 		// output_error();
 	}
 	return (0);
