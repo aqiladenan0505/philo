bf2beb4a1be1ffc3dda9e513004e88bee9019d24
Aqil Adenan <aadenan@c2r6s8.42singapore.sg>
2024-06-20 12:44:42 +0800

    Updated main

--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: aadenan <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 12:12:14 by aadenan           #+#    #+#             */
-/*   Updated: 2024/06/26 17:50:59 by aadenan          ###   ########.fr       */
+/*   Updated: 2024/06/26 18:41:27 by aadenan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,17 @@
 int	main(int argc, char **argv)
 {
 	t_data	data;
+
 	if (argc == 5 || argc == 6)
 	{
-		if (parse_input(&data, argv) == 1)
+		if (parse_input(argv) == 1)
 			return (0);
-		printf("All input is valid\n");
+		printf("Input is correct\n");
 		printf("philo_nbr: %d\n", data.philo_nbr);
 		printf("time_to_die: %d\n", data.time_to_die);
 		printf("time_to_eat: %d\n", data.time_to_eat);
 		printf("time_to_sleep: %d\n", data.time_to_sleep);
 		printf("meals_nbr: %d\n", data.meals_nbr);
-	if (argc == 5 || argc == 6)
-	{
-		parse_input(argv);
-		printf("Input is correct\n");
 		// data_init();
 		// execute_philo();
 		// free_all();
--- philo.h
+++ philo.h
@@ -6,11 +6,7 @@
 /*   By: aadenan <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 14:48:39 by aadenan           #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2024/06/20 14:09:21 by aadenan          ###   ########.fr       */
-=======
-/*   Updated: 2024/06/18 14:52:24 by aadenan          ###   ########.fr       */
->>>>>>> 316c3a0 (Added header file)
+/*   Updated: 2024/06/26 18:42:12 by aadenan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +15,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-<<<<<<< HEAD
 #include <limits.h>
-=======
->>>>>>> 316c3a0 (Added header file)
 #include <pthread.h>
 #include <sys/time.h>
 // #include <sys/types.h> // To check again
@@ -30,7 +23,6 @@
 
 typedef struct s_data
 {
-<<<<<<< HEAD
 	int	philo_nbr;
 	int	time_to_eat;
 	int	time_to_sleep;
@@ -39,8 +31,4 @@ typedef struct s_data
 } t_data;
 
 int	parse_input(t_data *data, char **argv);
-=======
-	// TO DO
-} t_data
 
->>>>>>> 316c3a0 (Added header file)
