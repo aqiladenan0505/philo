de344af83584249974b781dc1a1d6fe7f951ad37
Aqil Adenan <aadenan@c2r6s8.42singapore.sg>
2024-06-20 12:44:42 +0800

    Updated main

--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: aadenan <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 12:12:14 by aadenan           #+#    #+#             */
-/*   Updated: 2024/06/18 14:57:38 by aadenan          ###   ########.fr       */
+/*   Updated: 2024/06/20 11:46:25 by aadenan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,17 @@
 
 int	main(int argc, char **argv)
 {
+	t_data	data;
 	if (argc == 5 || argc == 6)
 	{
-		parse_input(argv);
+		if (parse_input(argv) == 1)
+			return (0);
 		printf("Input is correct\n");
+		printf("philo_nbr: %d\n", data.philo_nbr);
+		printf("time_to_die: %d\n", data.time_to_die);
+		printf("time_to_eat: %d\n", data.time_to_eat);
+		printf("time_to_sleep: %d\n", data.time_to_sleep);
+		printf("meals_nbr: %d\n", data.meals_nbr);
 		// data_init();
 		// execute_philo();
 		// free_all();
